9f8d97dea0f7ab8d7b2aec4f39bbcd718d479a7d
pigporket <pigporket@163.com>
2019-11-27 15:48:54 +0800

    Update push-images.sh

--- push-images.sh
+++ push-images.sh
@@ -3,10 +3,10 @@
 #change the registry name and username/password to yourself's.
 
 DOCKER_HUB=pigporket
-$ALI_REGISTRY=registry.cn-beijing.aliyuncs.com/alisop23
+ALI_REGISTRY=registry.cn-beijing.aliyuncs.com/alisop23
 
 #config the following env at travis-ui
-#docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker login registry.cn-beijing.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
 
 for image in $(cat img-list.txt)
@@ -14,9 +14,9 @@ do
 	imagename=$(echo $image | awk -F '/' '{print $NF}')
 	docker pull $image
 	docker tag $image $ALI_REGISTRY/$imagename
-#	docker tag $image $DOCKER_HUB/$imagename
+	docker tag $image $DOCKER_HUB/$imagename
 	# push到dockerhub
-#	docker push $DOCKER_HUB/$imagename
+	docker push $DOCKER_HUB/$imagename
 	# push到阿里云仓库
 	docker push $ALI_REGISTRY/$imagename
 done
