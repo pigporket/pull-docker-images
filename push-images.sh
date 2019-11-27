72b847967d2d0bb5de6afa681bcbc4d9b6b56638
pigporket <pigporket@163.com>
2019-11-27 15:19:23 +0800

    Update push-images.sh

--- push-images.sh
+++ push-images.sh
@@ -6,8 +6,8 @@ DOCKER_HUB=pigporket
 $ALI_REGISTRY=registry.cn-beijing.aliyuncs.com/alisop23
 
 #config the following env at travis-ui
-#docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-#docker login registry.cn-beijing.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login registry.cn-beijing.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
 
 for image in $(cat img-list.txt)
 do
