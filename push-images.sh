27e368bfd7a6d7d6c24ef1b4ca06f9fcae3c9a0b
pigporket <pigporket@163.com>
2019-11-27 14:52:07 +0800

    twice times

--- push-images.sh
+++ push-images.sh
@@ -7,7 +7,7 @@ ALI_REGISTRY=registry.cn-beijing.aliyuncs.com/alipigporket
 
 #config the following env at travis-ui
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker login registry.cn-hangzhou.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
+docker login registry.cn-beijing.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
 
 for image in $(cat img-list.txt)
 do
